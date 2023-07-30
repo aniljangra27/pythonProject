a51ce3241bbaca2c22d5c9f252c85017c32242d3
Anil Kumar <anil.kumar@akqa.com>
2023-07-30 21:42:15 +0530

    files addded

--- BasicPrint.py
+++ BasicPrint.py
@@ -1,1 +1,6 @@
 print("Hi, this is my first Python 3.11 program")
+
+print(1231321)
+
+print(1.0)
+
---
+++ variables.py
@@ -0,0 +1,15 @@
+# Multiple variable with different values
+
+a, b, c = "Apple", "Banana", "Cat"
+
+print(a)
+print(b)
+print(c)
+
+# Multiple variable with same values
+
+x = y = z = "Ram"
+
+print(x)
+print(y)
+print(z)
